9694a0424a5cc85c14192e5952ad02c858786e14
Glen Larkin <glenlarkin@gmail.com>
2020-09-22 16:29:52 -0600

    1.1 - Proper Comments

--- Find Oscar test.py
+++ Find Oscar test.py
@@ -1,6 +1,9 @@
+#! python3
+#Find Oscar Test.py - finds the name Oscar, in any case within a sentence
+
 import re
-def oscarFinder():
-    text = re.compile(r'oscar', re.I)
+def oscarFinder():              #need to make modular pls, accept string
+    text = re.compile(r'oscar', re.I)   #re.I makes it find any case
     print(text.search('Where the fuck is Oscar? maybe hes hidden in this text'))
 
 print(oscarFinder())
