5eefcf9713a048b1eb65fe08e36901b64408cf34
Glen Larkin <glenlarkin@gmail.com>
2020-09-22 16:56:46 -0600

    3.0 - User inputs string, and program finds oscar within it

--- Find Oscar test.py
+++ Find Oscar test.py
@@ -3,7 +3,8 @@
 
 import re
 def oscarFinder(oscar):                 #Accepts String from user to find oscar in
-    text = re.compile(r'oscar', re.I)   #re.I makes it find any case.
-    print(text.search(oscar))           #Takes text that user needs to find oscar in
-
-print(oscarFinder('Where is my Oscar? I cant seem to find him'))
+    text = re.compile(r'oscar', re.I)   #re.I helps us find any case.
+    print(text.findall(oscar))          #Takes text that user needs to find oscar in
+    
+userInput = input('Input text about Oscar\n') #Allows the user to input text about Oscar
+print(oscarFinder(userInput))                 #Displays the match within the text
