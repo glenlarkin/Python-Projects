2504b73349a80b967be2e6e1fde08a4dac318022
Glen Larkin <glenlarkin@gmail.com>
2020-09-23 20:31:36 -0600

    Annoy your idiot friends with this program!

---
+++ Input Validation/Busy idiot.py
@@ -0,0 +1,11 @@
+#! python3
+# How to Keep an Idiot Busy for Hours...
+
+import pyimputplus as pyip
+
+while True:
+    prompt = 'Want to know how to keep an idiot busy for hours?\n'
+    response = pyip.inputYesNo(prompt)
+    if response = 'no':
+        break
+        print('Thank you. Have a nice day.')
