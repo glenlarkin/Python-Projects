cfb692f5a4e176e5218bd3611b23ad4d943430bc
Glen Larkin <glenlarkin@gmail.com>
2020-10-21 11:06:06 -0600

    1.1 - added guessing element

--- Computer Games/coinFlips.py
+++ Computer Games/coinFlips.py
@@ -1,6 +1,6 @@
 import random
-print('I will flip a coin 1000 times. guess how many times it will come up heads. (Press enter to begin)')
-input()
+print('I will flip a coin 1000 times. guess how many times it will come up heads. (Press enter your guess to begin)')
+guess = input()
 flips = 0
 heads = 0
 while flips < 1000:
@@ -14,7 +14,11 @@ while flips < 1000:
         print('After 100 tosses, heads has come up ' + str(heads) +  ' times so far')
     if flips == 500:
         print('Halfway there! Update: heads has come up ' + str(heads) + ' times.')
+    guessAnswer = heads - int(guess)
+    if guessAnswer < 0:
+        guessAnswer = guessAnswer * -1
 
 print()
 print('Out of 1000 coin tosses, heads came up a total of ' + str(heads) + ' times!')
+print('You guessed ' + guess + ' You were ' + str(guessAnswer) + ' off of the result..')
 print('Were you close?')
