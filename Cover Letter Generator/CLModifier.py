c21cfa04ecae24358fa81d8c2b94965cd8e6188d
Glen Larkin <glenlarkin@gmail.com>
2020-11-06 14:18:37 -0700

    1.0 - Modifies a current template to inputs in Term

--- Cover Letter Generator/CLModifier.py
+++ Cover Letter Generator/CLModifier.py
@@ -1,8 +1,9 @@
 #! python3
-# coverLetter.py - Modify a master cover letter template to match the qualifications needed for a job.
+# CLModifier.py - Modify a Template Cover Letter to match the qualifications needed for a job.
 
 from docxtpl import DocxTemplate
 import datetime
+from pathlib import Path
 
 companyName = input('Which Company are you applying to?\n')
 positionName = input('What Position are they filling?\n')
@@ -13,10 +14,13 @@ todayDate = datetime.datetime.today().strftime('%m/%d/%Y')
 contextDict = { 'todayDate': todayDate, 'companyName': companyName, 'postionName': positionName}
 
 # Open Master CoverLetter template
-doc = DocxTemplate("master_cover_letter.docx")
+doc = DocxTemplate("masterCoverLetter.docx")
 
 # Load Variable names into file and replace template
 doc.render(contextDict)
 
 # Save the file with Modified filename
-doc.save('Cover_Letter_' + companyName + '_' + positionName + '.docx')
+doc.save(companyName + '_' + positionName + '_CoverLetter.docx')
+
+path = Path.cwd()
+print('Success!\nYour Cover Letter has been created and has been placed in: ' + str(path))
