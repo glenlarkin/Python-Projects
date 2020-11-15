acfc499cfd53410999fdb8915a46747b6933a980
Glen Larkin <glenlarkin@gmail.com>
2020-11-14 21:36:49 -0700

    working with zip files

--- Path/zip and unzip/extractZip.py
+++ Path/zip and unzip/extractZip.py
@@ -1,8 +1,8 @@
-import zipfile, os
+import zipfile
 from pathlib import Path
 
 p = Path.home()
 
-exampleZip = zipfile.ZipFile(p / 'example.zip')
+exampleZip = zipfile.ZipFile(p / 'Automate_the_Boring_Stuff_2e_onlinematerials.zip')
 exampleZip.extractall() #Can be used to place zip in existing or new file
 exampleZip.close()
