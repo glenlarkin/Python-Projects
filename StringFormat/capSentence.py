b7484b602cf40d31eaa972f1e508716af6a1a3ba
Glen Larkin <glenlarkin@gmail.com>
2020-11-16 13:56:49 -0700

    copy to clipboard

--- StringFormat/capSentence.py
+++ StringFormat/capSentence.py
@@ -1,5 +1,6 @@
 #!python3
 # Lower cases all words in string, except the first word in the sentence
+import pyperclip
 
 context = input('Enter any words you would like to De-Capitalize:\n')
 
@@ -8,4 +9,5 @@ allLow = context.lower()
 sentenceStart = allLow[0].capitalize()
 sentenceEnd = allLow[1:]
 result = sentenceStart + sentenceEnd
+pyperclip.copy(result)
 print(result)
--- StringFormat/capWord.py
+++ StringFormat/capWord.py
@@ -1,15 +1,9 @@
 #!python3
 # Capitalizes the first letter of each word given to it
-from tkinter import Tk
+import pyperclip
 
 context = input('Enter any words you would like to Capitalize\n')
 
 result = context.title()
+pyperclip.copy(result)
 print(result)
-
-r = Tk()
-r.withdraw()
-r.clipboard_clear()
-r.clipboard_append(result)
-r.update() # now it stays on the clipboard after the window is closed
-r.destroy()
