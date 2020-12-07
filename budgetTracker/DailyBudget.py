f27c613ff89274a1101c69eab7e32640c57c5224
GlenLarkin <glenlarkin@gmail.com>
2020-12-07 12:25:52 -0700

    implemented savings into final equation

--- budgetTracker/DailyBudget.py
+++ budgetTracker/DailyBudget.py
@@ -4,5 +4,7 @@
 income = input('How many Hours did you Work Today?\n')
 rate = input('How much did you make per hour\n')
 
-result = int(rate) * int(income)
-print('Today you made: $' + str(result))
+total = int(rate) * int(income)
+print('Today you made: $' + str(total))
+saving = total * 0.15
+print('If you save 85%, you may spend: $' + str(saving) + ' today')
