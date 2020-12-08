b2b6be4733de68d8414f0e78bd88ea0309a0ceb8
Glen Larkin <glenlarkin@gmail.com>
2020-12-07 17:13:31 -0700

    rates are variables, string conversion on functions

--- budgetTracker/biWeekly.py
+++ budgetTracker/biWeekly.py
@@ -1,14 +1,17 @@
 #!Python3
 # In takes users weekly income, and then creates a daily alotment to spend.
 
+appleRate = 0
+apwRate = 0
+
 def totalPay():
-    applePay = int(apple) * 0
-    apwPay = int(apw) * 0
+    applePay = int(apple) * appleRate
+    apwPay = int(apw) * apwRate
     return applePay + apwPay
     
 def afterTax():
-    appleTax = int(apple) * 0
-    apwTax = int(apw) * 0
+    appleTax = int(apple) * appleRate * 0.6
+    apwTax = int(apw) * apwRate * 0.8
     return appleTax + apwTax 
 
 saving = input('What Percentage of money would you like to save:  (0.1 = 10%) \n')
@@ -25,5 +28,6 @@ daily = oneWeek / 7
 print('--------------------------------')
 
 print('$' + str(twoWeekTotal) + ' total earned')
+print('After Taxes you made: $' + str(afterTax()))
 print('If you spend $' + str(daily) + ' each day')
 print('You will have saved: $' + str(savings))
