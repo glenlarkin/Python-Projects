6bf3e1a9fa2d02de4182134bc172933a43f9f9fc
Glen Larkin <glenlarkin@gmail.com>
2020-12-08 20:02:37 -0700

    Ch4 Custom Meme Creator, Neat Idea

--- GUIZero/memeMaker.py
+++ GUIZero/memeMaker.py
@@ -1,6 +1,46 @@
-from guizero import App, TextBox, Drawing
+# Imports ---------------
+from guizero import App, TextBox, Drawing, Combo, Slider
 
-app = App("meme")
-top_text = TextBox(app, "top text")
-bottom_text = TextBox(app, "bottom text")
+# Functions -------------
+def draw_meme():
+    meme.clear()
+    meme.image(0, 0, "C:/Users/glenl/Documents/GitHub/Python-Projects/GUIZero/Pizza.png")
+    
+    meme.text(
+        20, 20, top_text.value,
+        color=color.value,
+        size=size.value,
+        font=font_type.value)
+    meme.text(
+        20, 320, bottom_text.value,
+        color=color.value,
+        size=size.value,
+        font=font_type.value,
+        )
+
+# App -------------------
+app = App("Meme Generator")
+
+
+# Widgets ---------------
+top_text = TextBox(app, "top text", command=draw_meme)
+bottom_text = TextBox(app, "bottom text", command=draw_meme)
+
+color = Combo(app,
+    options=['black','white','red','green','blue','orange'], 
+    command=draw_meme,
+    selected='blue')
+
+font_type = Combo(app,
+    options=['times new roman','verdana','courier','impact'],
+    command=draw_meme,
+    selected='verdana')
+
+size = Slider(app, start=20, end=40, command=draw_meme)
+
+meme = Drawing(app, width='fill', height='fill')
+
+
+# Display ---------------
+draw_meme()
 app.display()
