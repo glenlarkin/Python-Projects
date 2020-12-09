98203a8ec8b4fe2bb325288575e0dae146de4ef3
Glen Larkin <glenlarkin@gmail.com>
2020-12-09 12:58:29 -0700

    Ch5 Worst chapter ever, bad UIs galore

--- GUIZero/badExample.py
+++ GUIZero/badExample.py
@@ -1,5 +1,11 @@
-from guizero import App, PushButton
+from guizero import App, PushButton, Text, Combo
+from string import ascii_letters
 
+def flashText():
+    if title.visible:
+        title.hide()
+    else:
+        title.show()
 
 def are_you_sure():
     if app.yesno("Confirmation", "Are you sure?"):
@@ -7,6 +13,15 @@ def are_you_sure():
     else:
         app.error("Ok", "Cancelling")
 app = App(title="pointless pop-ups")
+app.bg = 'dark green'
 button = PushButton(app, command=are_you_sure)
+title = Text(app, 'Dont have a seizure pls')
+a_letter = Combo(app, options=" " + ascii_letters, align="left")
+b_letter = Combo(app, options=" " + ascii_letters, align="left")
+c_letter = Combo(app, options=" " + ascii_letters, align="left")
+
 app.info("Application started", "Well done you started the application")
+
+app.repeat(1000, flashText)
+
 app.display()
