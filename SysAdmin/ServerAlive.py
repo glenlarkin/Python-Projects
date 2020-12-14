73633c35a0d91cfd1e643baeac46e603513d84be
Glen Larkin <glenlarkin@gmail.com>
2020-12-13 18:26:52 -0700

    Update ServerAlive.py

--- SysAdmin/ServerAlive.py
+++ SysAdmin/ServerAlive.py
@@ -3,22 +3,25 @@
 
 import os, csv
 
-with open('SysAdmin/apwServers.csv') as csvfile:
+with open('SysAdmin/larkinServers.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
 
     # Convert Server IPs to list
     newList = list(reader)
+    print(newList.count(newList))
     output = ''
 
     # Loop through CSV List
-    for row in newList:
-        ipAddr = row[0]
+    for column in newList:
+        ipAddr = column[0]
 
         # Ping each server
-        response = os.system('ping -c 2 ' + ipAddr)
+        response = os.system('ping -n 1 ' + ipAddr)
         if response == 0:
+            print(response)
             output += ipAddr + ' is up :)\n'
         else:
-            output += ipAddr + ', is down :(\n'
+            print(response)
+            output += ipAddr + ' is down :(\n'
 
 print(output + 'have a nice day')
