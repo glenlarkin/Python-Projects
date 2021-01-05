c176f5636163d226926b101704e52d24f8821fef
GlenLarkin <glenlarkin@gmail.com>
2021-01-05 13:57:12 -0700

    New Figlet fonts are fun

--- StringFormat/figlet.py
+++ StringFormat/figlet.py
@@ -1,4 +1,36 @@
 from pyfiglet import Figlet
 
-f = Figlet(font='slant')
-print(f.renderText('fuck you credit-cards'))
+text = "Fuck"
+
+larry3d = Figlet(font='larry3d')
+print(larry3d.renderText(text))
+
+isometric = Figlet(font='isometric2')
+print(isometric.renderText(text))
+
+ogre = Figlet(font='ogre')
+print(ogre.renderText(text))
+
+roman = Figlet(font="roman")
+print(roman.renderText(text))
+
+rozzo = Figlet(font="rozzo")
+print(rozzo.renderText(text))
+
+slant = Figlet(font="slant")
+print(slant.renderText(text))
+
+slscript = Figlet(font="slscript")
+print(slscript.renderText(text))
+
+speed = Figlet(font="speed")
+print(speed.renderText(text))
+
+usaflag = Figlet(font="usaflag")
+print(usaflag.renderText(text))
+
+ticks = Figlet(font="ticks")
+print(ticks.renderText(text))
+
+starwars = Figlet(font="starwars")
+print(starwars.renderText(text))
