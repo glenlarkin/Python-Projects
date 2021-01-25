2136c63348b1bb08e6993676eaa3090459dde5dd
GlenLarkin <glenlarkin@gmail.com>
2021-01-25 13:23:13 -0700

    autoEmailer from apw

---
+++ AutoEmail/sendEmail.py
@@ -0,0 +1,8 @@
+import ezgmail, csv
+print('sending from ' + ezgmail.EMAIL_ADDRESS)
+
+recipient = "glenlarkin@gmail.com"
+
+#ezgmail.send(recipient, 'Subject: Hello from work Glen Email', 'This is the body of the email, there is new content in here')
+
+print('Your email was sent to: ' + recipient)
---
+++ AutoEmail/unreadEmail.py
@@ -0,0 +1,26 @@
+import ezgmail
+
+#Checks work email for unread emails
+
+unreadThreads = ezgmail.unread()
+
+ezgmail.summary(unreadThreads)
+
+# Reads number of unread threads
+print('You have ' + str(len(unreadThreads)) + ' unread thread(s) in your work email')
+
+# Prints first unread email with all contents
+print(str(unreadThreads[0].messages[0]))
+
+# Prints subject of first unread email
+print(unreadThreads[0].messages[0].subject)
+
+# Prints body of first unread email
+print(unreadThreads[0].messages[0].body)
+
+print(unreadThreads[0].messages[0].timestamp)
+
+print(unreadThreads[0].messages[0].sender)
+
+print(unreadThreads[0].messages[0].recipient)
+ezgmail.send('glenlarkin@gmail.com', 'You have ' + str(len(unreadThreads)) + ' unread thread(s) in your work email', 'Please read your unread threads from' + unreadThreads[0].messages[0].sender)
