5503f5aeac4218d52d6c8d918c42d5c86c7455c3
Glen Larkin <glenlarkin@gmail.com>
2021-03-15 23:58:12 -0600

    adding "views"

---
+++ Json/iPhone Repair/startRepair.py
@@ -0,0 +1,19 @@
+import Repair
+import json
+import time
+
+data = json.loads(open('Json/iPhone Repair/iPhone.json').read())
+iPhones = data["iPhones"]
+
+print("Hello User!")
+print("")
+
+# Choose what device you will repair
+
+# Start Timer
+
+# Write a note?
+
+# save repair to database
+
+#
