ca12c1f13efc8d01ff63f9f561b0b88871b397dd
Glen Larkin <glenlarkin@gmail.com>
2021-03-25 10:11:06 -0600

    creating databases

--- Database/firstsqlite.py
+++ Database/firstsqlite.py
@@ -2,7 +2,7 @@ import sqlite3
 from sqlite3 import Error
  
 def create_connection(path):
-    connection = create_connection(/Users/sketchmaster/Coding/test.db)
+    connection = create_connection('/Users/sketchmaster/Documents/GitHub/Python-Projects/Database/test.sqlite')
     try:
         connection = sqlite3.connect(path)
         print("Connection to SQLite DB successful")
