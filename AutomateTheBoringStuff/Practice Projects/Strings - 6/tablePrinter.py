81533c230a6144b878063a0cc1699b6980802ede
Glen Larkin <glenlarkin@gmail.com>
2021-03-26 13:19:59 -0600

    outlining issue

---
+++ AutomateTheBoringStuff/Practice Projects/Strings - 6/tablePrinter.py
@@ -0,0 +1,9 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+['Alice', 'Bob', 'Carol', 'David'],
+['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(list):
+    colWidths = [0]* len(tableData)
+    print(colWidths)
+
+printTable(tableData)
