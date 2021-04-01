61c8fd2128adbbf39cfad830969c153a938f8125
Glen <glenlarkin@gmail.com>
2021-04-01 13:42:59 -0600

    crypto api call

---
+++ Crypto/cryptoCompare.py
@@ -0,0 +1,18 @@
+import cryptocompare
+
+usd = 'USD'
+ada = 'ADA'
+price = 0
+
+def priceFinder(token):
+    coinDict = cryptocompare.get_price(token, usd)
+    price = coinDict[token][usd]
+    for key in coinDict:
+        name = key
+    print('-' *50)
+    print('The price of ' + name + ' is currently ' + '$' + str(price))
+    print('-'*50)
+    
+    
+
+priceFinder(ada)
