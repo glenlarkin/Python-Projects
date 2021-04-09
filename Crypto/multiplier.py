cce726876745d54c1739aa5d0000f8e686bfaabe
Glen <glenlarkin@gmail.com>
2021-04-09 12:49:01 -0600

    endless loop

--- Crypto/multiplier.py
+++ Crypto/multiplier.py
@@ -6,9 +6,10 @@ print(speed.renderText(text))
 
 multiples = [5, 10, 20, 30, 50, 100]
 
-num = input('What number would you like to multiply?\n---------------------------------------\n')
-print('-'*15)
-for x in multiples:
-    result = int(num) * x
-    print(num + ' X ' + str(x) +' = ' + str(result))
+while True:
+    num = input('What number would you like to multiply?\n---------------------------------------\n')
     print('-'*15)
+    for x in multiples:
+        result = float(num) * x
+        print(num + ' X ' + str(x) +' = ' + str(result))
+        print('-'*15)
