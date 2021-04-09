33bdad051d9ead99223fd39392580e99125bbd70
Glen Larkin <glenlarkin@gmail.com>
2021-04-09 06:30:44 -0600

    fancier graphics

--- Crypto/multiplier.py
+++ Crypto/multiplier.py
@@ -6,9 +6,12 @@ print(speed.renderText(text))
 
 multiples = [5, 10, 20, 30, 50, 100]
 
-num = input('What number would you like to multiply?\n---------------------------------------\n')
-print('-'*15)
-for x in multiples:
-    result = int(num) * x
-    print(num + ' X ' + str(x) +' = ' + str(result))
+while True:
+    num = input('What number would you like to multiply?\n---------------------------------------\n')
     print('-'*15)
+    for x in multiples:
+        result = float(num) * x
+        print(num + ' X ' + str(x) +' = ' + str(result))
+        print('-'*15)
+    print('Hit ctrl-c to quit, I hope you acheive your gains!!!')
+    print('')
