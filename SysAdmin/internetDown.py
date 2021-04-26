9e676ed1d7dc3417351e06280ad274fc6c3869f9
Glen <glenlarkin@gmail.com>
2021-04-26 12:50:31 -0600

    removing supporting print comments

--- SysAdmin/internetDown.py
+++ SysAdmin/internetDown.py
@@ -16,34 +16,24 @@ def internetDown():
     down += 2
     global totalDown
     totalDown += 2
-    print('Internet has been down for: ' + str(down) + ' seconds\nTotal time down: ' + str(totalDown))
+    print('Current Downtime: ' + str(down) + ' seconds\nTotal Downtime: ' + str(totalDown) + ' seconds')
 
 def checkInternet():
-    print("starting checkinternet()")
-    print('getting date')
     currentTime = datetime.datetime.now()
     
     try:
-        print('trying comcast')
         r = requests.get('https://api.github.com/events')
         connected = str(r)
         
     except:
-        print('exception caught')
         log = open("InternetLog.txt", "a")
         log.write(str(currentTime) + ' No Internet\n')
         print(str(currentTime) + ' No Internet')
-        print('right b4 log close')
         log.close()
-        print('log has closed')
         
     if connected == '<Response [403]>' or '<Response [200]>':
-        
         internetUp()
-    print('ending checkinternet()')
-
-
-
+    
 while True:
     signal.signal(signal.SIGALRM, signal_handler)
     signal.alarm(10)   # Ten seconds
