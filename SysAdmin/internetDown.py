f1159ab55fba96227fcdd8b56131acea0108f03d
Glen <glenlarkin@gmail.com>
2021-04-26 10:28:23 -0600

    Second try/except checks to see when internet came back on

--- SysAdmin/internetDown.py
+++ SysAdmin/internetDown.py
@@ -1,18 +1,26 @@
 import requests, time, datetime
 
-down = 0
+url = 'https://comcast.com'
+
 
 def checkInternet():
+    down = 0
     currentTime = datetime.datetime.now()
     
     try:
-        r = requests.get('https://api.github.com/events')
+        r = requests.get(url)
         print(str(currentTime) + " Connected")
     except:
         log = open("InternetLog.txt", "a")
         log.write(str(currentTime) + ' No Internet\n')
         log.close()
         print(str(currentTime) + ' No Internet')
+        try:
+            r = requests.get(url)
+            print("Internet has come back")
+        except:
+            
+            down += 1
         
 
 while True:
