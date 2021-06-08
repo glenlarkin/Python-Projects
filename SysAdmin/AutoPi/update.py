a0751d5ceacbca9aa1caf35857db5c90097aaa3e
Glen <glenlarkin@gmail.com>
2021-06-08 11:21:01 -0600

    Automatically check and run updates. needs logging

---
+++ SysAdmin/AutoPi/update.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# Checks repositories for updates, lists updates, then run update if desired
+import os
+
+print('Checking for Updates to run')
+
+sudo_password = 'fuck0ff'
+
+command = 'apt-get update'
+os.system('echo %s|sudo -S %s' % (sudo_password, command))
+
+command = 'apt list --upgradeable'
+updates = os.system(command)
+
+if updates == 0:
+    print('No updates to run')
+
+else:
+    print("Updates are available \n" + updates )
+
+    question = input("Would you like to update? ")
+    if question == 'y' or question == 'yes':
+        
+        command = 'apt-get upgrade -y'
+        os.system('echo %s|sudo -S %s' % (sudo_password, command))
+    else:
+        print("Update Canceled")
