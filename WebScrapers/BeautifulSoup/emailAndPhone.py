01fa9b08b7404b61f9feb6689c3717769c589de5
Glen Larkin <47052711+GlenLarkin@users.noreply.github.com>
2021-09-22 19:55:27 -0600

    Sorting results alphabetically

--- WebScrapers/BeautifulSoup/emailAndPhone.py
+++ WebScrapers/BeautifulSoup/emailAndPhone.py
@@ -2,10 +2,12 @@ import requests
 import re
 
 # get the data
-data = requests.get('http://sasbot.slcc.edu/directory/dept.aspx?deptid=QACCT')
+data = requests.get('https://research.utah.edu/about/staff.php')
 
 # extract the phone numbers, emails, whatever
 phones = re.findall(r'(\(?[0-9]{3}\)?(?:\-|\s|\.)?[0-9]{3}(?:\-|\.)[0-9]{4})', data.text)
 emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
 
-print(phones, emails)
+alpha_emails = sorted(emails)
+for e in alpha_emails:
+    print(e)
