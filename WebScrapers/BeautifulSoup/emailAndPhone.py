1c782438595f7b7572588f2240f88f0863972f7f
Glen Larkin <47052711+GlenLarkin@users.noreply.github.com>
2021-09-29 18:36:20 -0600

    Cleaner sort and no duplicates

--- WebScrapers/BeautifulSoup/emailAndPhone.py
+++ WebScrapers/BeautifulSoup/emailAndPhone.py
@@ -2,12 +2,16 @@ import requests
 import re
 
 # get the data
-data = requests.get('https://research.utah.edu/about/staff.php')
+data = requests.get('https://irb.utah.edu/about/contact/')
 
 # extract the phone numbers, emails, whatever
 phones = re.findall(r'(\(?[0-9]{3}\)?(?:\-|\s|\.)?[0-9]{3}(?:\-|\.)[0-9]{4})', data.text)
 emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
 
-alpha_emails = sorted(emails)
-for e in alpha_emails:
-    print(e)
+alpha_emails = list(set(sorted(emails)))
+
+#print(no_duplicates)
+for email in alpha_emails:
+    print(email)
+
+print(phones)
